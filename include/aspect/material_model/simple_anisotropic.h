96c338bd5ac54117f8d453706d8ba92322e4ab91
Jonathan Perry-Houts <jperryh2@uoregon.edu>
2015-06-22 16:02:09 -0700

    Add simple anisotropic material model

---
+++ include/aspect/material_model/simple_anisotropic.h
@@ -0,0 +1,77 @@
+/*
+  Copyright (C) 2015 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __aspect__model_simple_anisotropic_h
+#define __aspect__model_simple_anisotropic_h
+
+#include <aspect/material_model/simple.h>
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    /**
+     * A material model that consists of globally constant values for all
+     * material parameters except density and viscosity.
+     *
+     * Identical to Simple model, but also assigns viscosity tensor based
+     * on Parameter file input.
+     *
+     * @ingroup MaterialModels
+     */
+    template <int dim>
+    class SimpleAnisotropic : public MaterialModel::Simple<dim>
+    {
+      public:
+
+        virtual void evaluate(const MaterialModelInputs<dim> &in,
+                              MaterialModelOutputs<dim> &out) const;
+
+        /**
+         * @name Functions used in dealing with run-time parameters
+         * @{
+         */
+        /**
+         * Declare the parameters this class takes through input files.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+        /**
+         * @}
+         */
+
+      private:
+        SymmetricTensor<4,dim> C; // Constitutive tensor
+    };
+
+  }
+}
+
+#endif
