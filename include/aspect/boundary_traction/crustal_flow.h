ada2b5723356b4e219bf47e8c93648a831555528
Jonathan Perry-Houts <jon@than.ph>
2020-08-05 16:18:07 -0700

    no functionality, but compiles without errors

---
+++ include/aspect/boundary_traction/crustal_flow.h
@@ -0,0 +1,159 @@
+/*
+  Copyright (C) 2011 - 2020 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _aspect_boundary_traction_crustal_flow_h
+#define _aspect_boundary_traction_crustal_flow_h
+
+#include <deal.II/base/quadrature.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/timer.h>
+#include <deal.II/base/table_handler.h>
+#include <deal.II/base/utilities.h>
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/tensor.h>
+#include <deal.II/lac/generic_linear_algebra.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/solver_minres.h>
+#include <deal.II/lac/constraint_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/numerics/solution_transfer.h>
+#include <deal.II/lac/sparsity_tools.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/distributed/grid_refinement.h>
+#include <deal.II/distributed/solution_transfer.h>
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <csignal>
+
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#include <boost/iostreams/tee.hpp>
+#include <boost/iostreams/stream.hpp>
+
+#include <aspect/boundary_traction/interface.h>
+#include <aspect/simulator_access.h>
+#include <aspect/utilities.h>
+
+namespace aspect
+{
+  namespace BoundaryTraction
+  {
+    using namespace dealii;
+
+    /**
+     * A class that implements traction boundary conditions based on ...
+     *
+     * @ingroup BoundaryTractions
+     */
+    template <int dim>
+    class CrustalFlow : public Interface<dim>, public SimulatorAccess<dim>
+    {
+      public:
+        /**
+         * Constructor.
+         */
+        CrustalFlow ();
+
+        /**
+         * Initialization function. This function is called once at the
+         * beginning of the program. Checks preconditions.
+         */
+        void
+        initialize () override;
+
+        /**
+        * A function that is called at the beginning of each time step to
+        * indicate what the model time is for which the boundary values will
+        * next be evaluated. For the current class, the function passes to
+        * the parsed function what the current time is.
+        */
+        void update () override;
+
+        /**
+         * Return the boundary traction as a function of position. The
+         * (outward) normal vector to the domain is also provided as
+         * a second argument.
+         */
+        Tensor<1,dim>
+        boundary_traction (const types::boundary_id boundary_indicator,
+                           const Point<dim> &p,
+                           const Tensor<1,dim> &normal) const override;
+
+      private:
+        static constexpr unsigned int boundarydim = dim - 1;
+        parallel::distributed::Triangulation<boundarydim, dim> triangulation;
+        DoFHandler<boundarydim, dim> dof_handler;
+        FESystem<boundarydim, dim> fe;
+        ConstraintMatrix constraints;
+
+        IndexSet locally_owned_dofs;
+        IndexSet locally_relevant_dofs;
+
+        dealii::LinearAlgebraTrilinos::MPI::SparseMatrix system_matrix;
+        dealii::LinearAlgebraTrilinos::MPI::Vector locally_relevant_solution;
+        dealii::LinearAlgebraTrilinos::MPI::Vector old_locally_relevant_solution;
+        dealii::LinearAlgebraTrilinos::MPI::Vector system_rhs;
+
+        const FEValuesExtractors::Vector u_extractor;
+        const FEValuesExtractors::Scalar h_extractor;
+        const FEValuesExtractors::Scalar w_extractor;
+        const FEValuesExtractors::Scalar s_extractor;
+
+        void setup_dofs ();
+        void assemble_system (const double dt);
+        void solve ();
+        void refine_mesh ();
+        double get_dt ();
+
+        double rho_c=2650;
+        double rho_m=3300;
+        double rho_s=3550;
+
+        types::boundary_id surface_boundary_id;
+    };
+  }
+}
+
+#endif
---
+++ source/boundary_traction/crustal_flow.cc
@@ -0,0 +1,140 @@
+/*
+  Copyright (C) 2011 - 2020 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/boundary_traction/crustal_flow.h>
+#include <aspect/utilities.h>
+#include <aspect/global.h>
+
+namespace aspect
+{
+  namespace BoundaryTraction
+  {
+    template <int dim>
+    CrustalFlow<dim>::CrustalFlow ()
+      :
+      triangulation (MPI_COMM_WORLD,
+                     typename Triangulation<boundarydim, dim>::MeshSmoothing (
+                       Triangulation<boundarydim, dim>::smoothing_on_refinement | Triangulation<boundarydim, dim>::smoothing_on_coarsening),
+                     parallel::distributed::Triangulation<boundarydim, dim>::mesh_reconstruction_after_repartitioning),
+      dof_handler (triangulation),
+      fe (FE_Q<boundarydim, dim> (2), 2 /* Crustal flow velocity */,
+          FE_Q<boundarydim, dim> (1), 1 /* Crustal thickness */,
+          FE_Q<boundarydim, dim> (1), 1 /* Elastic plate deflection */,
+          FE_Q<boundarydim, dim> (1), 1 /* Overburden load */),
+      u_extractor (0),
+      h_extractor (2),
+      w_extractor (3),
+      s_extractor (4)
+    {}
+
+
+    template <int dim>
+    void
+    CrustalFlow<dim>::initialize ()
+    {
+      surface_boundary_id =
+        this->get_geometry_model().translate_symbolic_boundary_name_to_id("top");
+    }
+
+
+    template <int dim>
+    void
+    CrustalFlow<dim>::update()
+    {
+      std::set<types::boundary_id> boundary_ids;
+      boundary_ids.insert(surface_boundary_id);
+      GridGenerator::extract_boundary_mesh(this->get_triangulation(),
+                                           triangulation,
+                                           boundary_ids);
+
+      double model_timestep = this->get_timestep();
+      double dt;
+      double time = 0;
+      do
+        {
+          dt = get_dt();
+          assemble_system(dt);
+          solve();
+          time += dt;
+        }
+      while (time < model_timestep);
+    }
+
+
+    template <int dim>
+    Tensor<1,dim>
+    CrustalFlow<dim>::
+    boundary_traction (const types::boundary_id,
+                       const Point<dim> &,
+                       const Tensor<1,dim> &normal_vector) const
+    {
+      double traction = 0;
+      return traction * normal_vector;
+    }
+
+    template <int dim>
+    void
+    CrustalFlow<dim>::
+    setup_dofs()
+    {
+    }
+
+    template <int dim>
+    void
+    CrustalFlow<dim>::
+    assemble_system(const double)
+    {
+    }
+
+    template <int dim>
+    void
+    CrustalFlow<dim>::
+    solve()
+    {
+    }
+
+    template <int dim>
+    void
+    CrustalFlow<dim>::
+    refine_mesh()
+    {
+    }
+
+    template <int dim>
+    double
+    CrustalFlow<dim>::
+    get_dt()
+    {
+      return 1000 * 3.14e7;
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace BoundaryTraction
+  {
+    ASPECT_REGISTER_BOUNDARY_TRACTION_MODEL(CrustalFlow, "Crustal flow",
+                                            "Implementation of a model in which the boundary "
+                                            "traction is given in terms of ")
+  }
+}
